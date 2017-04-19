2ec32d770f2b5d56320b271fa2a7380925fc1fcb
DraganNinkovic <drninkovicns@gmail.com>
2017-04-19 08:07:21 +0200

    Zavrsena Walker klasa

--- wp-content/themes/mytheme/inc/walker.php
+++ wp-content/themes/mytheme/inc/walker.php
@@ -24,7 +24,7 @@
  */
 class Walker_Nav_Primary extends Walker_Nav_Menu
 {
-  function start_lvl(&$output, $depth){ // ul
+  function start_lvl(&$output, $depth = 0, $args = array()){ // ul
     $indent = str_repeat("\t", $depth);
     $submenu = ($depth > 0) ? 'sub-menu' : '';
     $output .= "\n$indent<ul class=\"dropdown-menu$submenu depth_$depth\">\n";
@@ -41,27 +41,43 @@ class Walker_Nav_Primary extends Walker_Nav_Menu
     $classes[] = ($args->walker->has_children) ? 'dropdown' : '';
     $classes[] = ($item->current || $item->current_item_ancestor) ? 'active' : '';
     $classes[] = 'menu-item-' . $item->ID;
-    if($depth && $args->has_children){
+    if($depth && $args->walker->has_children){
       $classes[] = 'dopdown-submenu';
     }
 
     $class_names = join(' ', apply_filters('nav_menu_css_class', array_filter($classes), $item, $args));
     $class_names = ' class="' . esc_attr($class_names). '"';
 
-    $id = apply_filters('nav_menu_item_id', 'menu-item-' . $item->id, $item, $args);
+    $id = apply_filters('nav_menu_item_id', 'menu-item-' . $item->ID, $item, $args);
     $id = strlen($id) ? ' id="' . esc_attr($id) . '"' : '';
 
-    $output .= $indent . '<li' . $id . $value . $class_names . $li_atrributes . '>';
-    // 30:00 minut 15. lekcije
+    $output .= $indent . '<li' . $id . $value . $class_names . $li_attribute . '>';
+
+    $attributes = !empty($item->attr_title) ? ' title="' . esc_attr($item->attr_title) . '"' : '';
+    $attributes .= !empty($item->target) ? ' target="' . esc_attr($item->target) . '"' : '';
+    $attributes .= !empty($item->xfn) ? ' rel="' . esc_attr($item->xfn) . '"' : '';
+    $attributes .= !empty($item->url) ? ' href="' . esc_attr($item->url) . '"' : '';
+
+    $attributes .= ($args->walker->has_children) ? ' class="dropdown-toggle" data-toggle="dropdown"' : '';
+
+    $item_output = $args->before;
+    $item_output .= '<a' . $attributes . '>';
+    $item_output .= $args->link_before . apply_filters('the_title', $item->title, $item->ID) . $args->link_after;
+    $item_output .= ($depth == 0 && $args->has_children) ? ' <b class="caret"></b></a>' : '</a>';
+    $item_output .= $args->after;
+
+    $output .= apply_filters('walker_nav_menu_start_el', $item_output, $item, $depth, $args);
+
+
   }
 
-  // function end_el(){ // closing li a span
-    //end element
-  // }
+//   function end_el(){ // closing li a span
+//    end element
+//   }
 
-  // function end_lvl(){ // closing ul
-    // end level
-  // }
+//   function end_lvl(){ // closing ul
+//     end level
+//   }
 
 
 }
